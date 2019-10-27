6572cf93e022582aa30c280c7a3fa030b2a1a3f8
misodengaku <misodengaku@gmail.com>
2019-10-27 09:05:17 +0000

    nazo

--- imagick/processors.go
+++ imagick/processors.go
@@ -7,14 +7,14 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-func (p *Processor) Get() *Worker {
-	p.limit <- struct{}{}
-	return p.pool.Get().(*Worker)
+func (p *Processor) Get() Worker {
+//	p.limit <- struct{}{}
+	return p.pool.Get().(Worker)
 }
 
-func (p *Processor) Put(w *Worker) {
+func (p *Processor) Put(w Worker) {
 	p.pool.Put(w)
-	<-p.limit
+//	<-p.limit
 }
 
 func (w *Worker) processGenericImage(path string) ([]byte, error) {
