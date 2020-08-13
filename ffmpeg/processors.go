c935ac895b36dc6280cd043e55c49b533bf948fb
misodengaku <misodengaku@gmail.com>
2020-08-13 13:47:17 +0900

    fix aspect ratio of movie thumbnail

--- ffmpeg/processors.go
+++ ffmpeg/processors.go
@@ -13,10 +13,8 @@ import (
 )
 
 func (p *Processor) getMP4Thumbnail(filename string, targetWidth uint) string {
-
 	hash := sha256.Sum256([]byte(filename))
 	fileid := filepath.Join(p.tempDir, hex.EncodeToString(hash[:])+".gif")
-	palette := filepath.Join(p.tempDir, hex.EncodeToString(hash[:])+".png")
 
 	skipSecond := 0
 	duration := 10
@@ -24,7 +22,7 @@ func (p *Processor) getMP4Thumbnail(filename string, targetWidth uint) string {
 	size := 300
 
 	// https://robservatory.com/easily-create-animated-gifs-from-video-via-ffmpeg/
-	filters := fmt.Sprintf("fps=%d,scale=%d:-1:flags=lanczos,palettegen", fps, size)
+	filters := fmt.Sprintf("fps=%d,scale=%d:%d:force_original_aspect_ratio=decrease:flags=lanczos,pad=%d:%d:(ow-iw)/2:(oh-ih)/2", fps, size, size, size, size)
 
 	cmd1 := []string{
 		"-ss", strconv.Itoa(skipSecond),
@@ -32,25 +30,8 @@ func (p *Processor) getMP4Thumbnail(filename string, targetWidth uint) string {
 		"-i", filename,
 		"-vf", filters,
 		"-y",
-		palette}
-
-	filters = fmt.Sprintf("fps=%d,scale=%d:-1:flags=lanczos [x]; [x][1:v] paletteuse", fps, size)
-
-	cmd2 := []string{
-		"-ss", strconv.Itoa(skipSecond),
-		"-t", strconv.Itoa(duration),
-		"-i", filename,
-		"-i", palette,
-		"-lavfi",
-		filters,
-		"-y",
-		fileid}
-
-	for _, v := range cmd2 {
-		fmt.Print(v)
-		fmt.Print(" ")
+		fileid,
 	}
-	fmt.Println("")
 
 	var out1 bytes.Buffer
 
@@ -62,23 +43,6 @@ func (p *Processor) getMP4Thumbnail(filename string, targetWidth uint) string {
 		log.Warn(err)
 		return ""
 	}
-
-	// cmd1Out, cmd1Err := exec.Command("/usr/bin/ffmpeg", cmd1...).Output()
-	// log.Info(cmd1Out, cmd1Err)
-	// cmd2Out, cmd2Err := exec.Command("/usr/bin/ffmpeg", cmd2...).Output()
-
-	log.Info("stage 2")
-	paletteToGifCmd := exec.Command("ffmpeg", cmd2...)
-	// paletteToGifCmd.Stdout = &out2
-	// err = paletteToGifCmd.Run()
-	cmd2Out, cmd2Err := paletteToGifCmd.Output()
-	log.Info(cmd2Out, cmd2Err)
-	if cmd2Err != nil {
-		log.Warn("stage2 failed: ", filename)
-		log.Warn(cmd2Err)
-		return ""
-	}
-
 
 	return fileid
 }
